f137797444cd01cce54d176b43764739638c4103
Lumither Tao <lumither@outlook.com>
2023-08-07 02:28:09 +0800

    <feat + test> [module: TaskList] update from() - update from() for creating a connection to database form given path   from config file - add test module for the feature above

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 pub mod config;
 pub mod task;
 
+mod test;
+
 fn run() {}
--- src/task/task_list.rs
+++ src/task/task_list.rs
@@ -1,11 +1,19 @@
 use rusqlite;
 
+#[derive(Debug)]
 pub struct TaskList {
     db_connection: rusqlite::Connection,
 }
 
 impl TaskList {
-    pub fn new() -> Self {
-        todo!("under dev")
+    pub fn from(db_path: &str) -> Result<Self, String> {
+        let tmp_db_conn = rusqlite::Connection::open(db_path);
+        if let Ok(tmp_db_conn) = tmp_db_conn {
+            Ok(TaskList {
+                db_connection: tmp_db_conn,
+            })
+        } else {
+            Err(tmp_db_conn.unwrap_err().to_string())
+        }
     }
 }
