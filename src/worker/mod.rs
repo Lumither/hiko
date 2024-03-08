cac5c3dc8723509ddf508c0aed56a5e948d22a56
Lumither Tao <github@lumither.com>
2024-03-08 23:22:45 +0800

    feat(executor) new guardian thread

--- src/worker/mod.rs
+++ src/worker/mod.rs
@@ -3,12 +3,15 @@ use std::sync::Arc;
 use crate::config::Config;
 use crate::database::tasks::TaskDB;
 use crate::worker::executor::task_executor;
+use crate::worker::guardian::guardian;
 
 mod executor;
+mod guardian;
 mod utils;
 
 pub async fn run(config: Config, task_db: Arc<TaskDB>) {
     let executor = tokio::spawn(task_executor(task_db.clone()));
+    let guardian = tokio::spawn(guardian());
 
-    let _ = tokio::join!(executor);
+    let _ = tokio::join!(guardian, executor);
 }
